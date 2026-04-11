2e5b4607ad297931e9fa9c3f99ece4145a080f70
Christian Pasero <pasero.christian99@gmail.com>
2026-04-11 09:49:11 +0200

    forgot small image transformation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
 
   has_one_attached :avatar do |attachable|
     attachable.variant :small,
-                       resize_to_fill: [200, 200],
-                       modulate: "85,60,100",
+                       resize_to_fill: [1000, 1000],
+                       modulate: "80,55,100",
                        colorize: "10,5,0",
-                       level: "15%,85%",
+                       level: "5%,90%",
                        contrast: true,
-                       blur: "0x1"
+                       blur: "0x1.5",
+                       paint: 3,
+                       colorspace: "Gray"
     attachable.variant :large,
                        resize_to_fill: [1000, 1000],
                        modulate: "80,55,100",
