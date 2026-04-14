c15e93d0a73f2650f815aa312af07da7fc28182e
Katja Daxberger <katja.daxi@gmail.com>
2026-04-14 20:03:25 +0200

    :((

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "https://inkloft-backend.p4s3r0.it"
+    origins "http://localhost:5173", "https://ink-loft.d4xika.com"
     resource "*",
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
