02e709990e8c17b626548830fb8e5b05e34bb313
Katja Daxberger <katja.daxi@gmail.com>
2026-04-14 18:58:02 +0200

    feat: added API_URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,41 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [PrimeVueResolver()],
-      dirs: ["src/components", "src/views/sections"],
-      extensions: ["vue"],
-      deep: true,
-      dts: true,
-    }),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Ink Loft",
-        theme_color: "red",
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [PrimeVueResolver()],
+        dirs: ["src/components", "src/views/sections"],
+        extensions: ["vue"],
+        deep: true,
+        dts: true,
+      }),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "Ink Loft",
+          theme_color: "#1c1917",
+        },
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        },
+      }),
+    ],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.API_URL || "http://127.0.0.1:3000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
