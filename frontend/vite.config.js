67f649830a17ff6ec22eff3fa7b5f60a40a25944
Katja Daxberger <katja.daxi@gmail.com>
2026-04-14 19:24:36 +0200

    feat: added API_URL (fix)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         "/api": {
-          target: env.API_URL || "http://127.0.0.1:3000",
+          target: "http://127.0.0.1:3000",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
