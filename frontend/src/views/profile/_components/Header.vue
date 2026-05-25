f037606eae3816af8d35ef735d1212178aa661eb
Katja Daxberger <katja.daxi@gmail.com>
2026-05-25 10:57:34 +0200

    feat: implement user authentication flow (frontend & backend)

--- frontend/src/views/profile/_components/Header.vue
+++ frontend/src/views/profile/_components/Header.vue
@@ -1,7 +1,21 @@
 <script setup>
 import { useRouter } from "vue-router";
+import API from "../../../helper/api.js";
+import { setAuthStatus } from "../../../router/router.js";
 
 const router = useRouter();
+
+function logout() {
+  API.put("users/logout").then(
+    () => {
+      setAuthStatus(false);
+      router.push({ name: "authentication" });
+    },
+    () => {
+      //TODO: toasty
+    },
+  );
+}
 </script>
 
 <template>
@@ -11,7 +25,12 @@ const router = useRouter();
       variant="square"
       @click="router.push({ name: 'home' })"
     />
-    <ILIconButton icon="pi-bell" variant="square" />
+    <ILIconButton
+      icon="pi-sign-out"
+      variant="square"
+      color="red"
+      @click="logout()"
+    />
   </div>
 </template>
 
