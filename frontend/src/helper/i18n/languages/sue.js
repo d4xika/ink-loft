deabdfb42b1772611d292b431af15f2fa3f3cece
Christian Pasero <pasero.christian99@gmail.com>
2026-06-20 20:01:45 +0200

    updated sue language translations and production host configuration

--- frontend/src/helper/i18n/languages/sue.js
+++ frontend/src/helper/i18n/languages/sue.js
@@ -1,5 +1,58 @@
 export const sue = {
+  general: {
+    delete: "Löschen",
+    submit: "Speichern",
+  },
   authentication: {
-    title: "Servus Lesor!",
+    title: "Servus Leser!",
+    login: "LOGIN",
+    register: "REGISTRIEREN",
+    email: "E-Mail",
+    username: "Benutzernome",
+    password: "Passwort",
+    confirm_password: "Passwort bestetign",
+  },
+  home: {
+    want_to_read: "Leseliste",
+    have_read: "Glesen",
+    finish_read: "Lektüre beendn",
+  },
+  quotes: {
+    quote: "Zitat",
+    save: "Zitat speichern",
+    add: "Zitat hinzuafügen",
+    edit: "Zitat beorbeiten",
+  },
+  read: {
+    title: "Titl",
+    author: "AutorIn",
+    cover: "Cover",
+    rating: "Bewertung",
+    start_date: "Startdatum",
+    end_date: "Enddatum",
+    would_recommend: "Empfehlung",
+    recommended: "Empfohlen",
+    not_recommended: "Net empfohlen",
+    platform: "Plottform",
+    chapters: "Kapitl",
+    words: "Werter",
+    pages: "Seitn",
+    pairing: "Poorung",
+    notes: "Notizn",
+    link: "Vorlinkung",
+    delete: "Lektüre löschen",
+    delete_confirmation:
+      "Bisch du dor sicher, dass du de Lektüre löschen willsch? Olle Zitate de dorzuaghearn werdn glöscht.",
+
+    reading_status: {
+      reading_status: "Lesestatus",
+      want_to_read: "Leseliste",
+      currently_reading: "Am Lesen",
+      finished: "Fertig glesen",
+      dropped: "Aufgheart",
+    },
+  },
+  profile: {
+    language: "Sproch",
   },
 };
