d9dd614f7aa4a1b7e2e57a5da8f21052d9631912
Christian Pasero <pasero.christian99@gmail.com>
2026-08-09 14:52:36 +0200

    me dumb

--- frontend/src/views/profile/ProfileView.vue
+++ frontend/src/views/profile/ProfileView.vue
@@ -2,10 +2,8 @@
 import { useToast } from "primevue/usetoast";
 import { ref } from "vue";
 import { useI18n } from "vue-i18n";
-import { languages } from "../../helper/i18n/i18n.js";
 import API from "@/helper/api.js";
 import { languages } from "@/helper/i18n/i18n.js";
-import { useToast } from "primevue/usetoast";
 
 const { locale } = useI18n();
 const { t } = useI18n();
